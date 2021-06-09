cfc4f186a845b8ec330ed452adfdb3489595379e
L2AsHdz <leonidas0197@gmail.com>
2021-06-08 23:49:40 -0600

    interfaz grafica agregada

--- dist/Main.js
+++
@@ -1,7 +0,0 @@
-function testTS() {
-    const texto = document.getElementById('inputText');
-    const consola = document.getElementById('areaInfo');
-    // @ts-ignore
-    let result = AnalyzerXML.parse(texto.value);
-    consola.value = result;
-}
--- dist/Nodo/Atributo.js
+++
@@ -1,8 +0,0 @@
-class Atributo {
-    constructor($nombre, $valor, $linea, $columna) {
-        this.nombre = $nombre;
-        this.valor = $valor;
-        this.linea = $linea;
-        this.columna = $columna;
-    }
-}
